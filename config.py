668963c4aedb1f8bac9c12c9a2b047fe7baa0a93
Vladislav Gritsay <114249677+veles008@users.noreply.github.com>
2022-12-12 23:19:44 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = '5589916263:AAHNHYW2UmC49ccaj8HhPwIjjhFMD15-k90'
+CURRENCY_API = 'c699c1bde1dd304c11cd646a54220fb3'
+val = {
+    'евро' : 'EUR',
+    'рубль' : 'RUB',
+    'доллар' : 'USD'
+}
+symbols = {
+    'EUR' : '€',
+    'RUB' : '₽',
+    'USD' : '$'
+}
